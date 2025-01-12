2d5975b84e57a9f766c58612842caba2b86047b7
Saman-naruee <samannaruee@gmail.com>
2025-01-12 11:49:58 +0330

    update emalls_api to try api style crawling.

--- emalls_shop/emalls_shop/spiders/emalls_api.py
+++ emalls_shop/emalls_shop/spiders/emalls_api.py
@@ -1,10 +1,20 @@
-import scrapy
+import scrapy  
+import json  
+from colorama import Fore, Back, Style
 
-
-class EmallsApiSpider(scrapy.Spider):
-    name = "emalls_api"
-    allowed_domains = ["emalls.ir"]
-    start_urls = ["https://emalls.ir/_Search.ashx"]
-
-    def parse(self, response):
-        pass
+class EmallsApiSpider(scrapy.Spider):  
+    name = "emalls_api"  
+    allowed_domains = ["emalls.ir"]  
+    start_urls = ["https://emalls.ir/_Search.ashx"]  
+    
+    def parse(self, response):  
+        response_body = response.text  
+        
+        try:  
+            data = json.loads(response_body)  
+            print(Fore.GREEN + str(response.status) + Style.RESET_ALL)  
+            print(Fore.BLUE, f"Response data: {str(data)}", Style.RESET_ALL)
+        except json.JSONDecodeError:  
+            self.logger.error("Failed to decode JSON response")  
+        except Exception as e:  
+            self.logger.error(f"An error occurred: {e}")  
--- emalls_shop/emalls_shop/spiders/single_shop_products.py
+++ emalls_shop/emalls_shop/spiders/single_shop_products.py
@@ -10,17 +10,6 @@ class SingleShopProductsSpider(scrapy.Spider):
     allowed_domains = ["emalls.ir"]
     start_urls = [f"https://emalls.ir/%D9%84%DB%8C%D8%B3%D8%AA-%D9%82%DB%8C%D9%85%D8%AA~shop~{generate_token()}"]
 
-import scrapy
-from ..items import EmallsShopItems
-
-def generate_token():
-    return '21766'
-
-class SingleShopProductsSpider(scrapy.Spider):
-    name = "single_shop_products"
-    allowed_domains = ["emalls.ir"]
-    start_urls = [f"https://emalls.ir/%D9%84%DB%8C%D8%B3%D8%AA-%D9%82%DB%8C%D9%85%D8%AA~shop~{generate_token()}"]
-
     def parse(self, response):
         item = EmallsShopItems()
         item['products'] = []
