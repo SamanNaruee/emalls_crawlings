9b400525f78de67135239641b9f452c75ec5642c
Saman-naruee <samannaruee@gmail.com>
2025-01-12 11:12:39 +0330

    update model and admin to save each shop data including products.

--- Dj_shops/admin.py
+++ Dj_shops/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import EmallsShop, Product
 
-# Register your models here.
+
+@admin.register(EmallsShop)
+class EmallsShopAdmin(admin.ModelAdmin):
+    list_display = ['name', 'url', 'price']
+    search_fields = ['name', 'url', 'price']
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['name', 'url', 'image_url', 'store_count', 'price', 'product_id']
+    search_fields = ['name', 'url', 'image_url', 'store_count', 'price', 'product_id']
--- Dj_shops/models.py
+++ Dj_shops/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
-# Create your models here.
+class EmallsShop(models.Model):
+    name = models.CharField(max_length=100, null=True, blank=True)
+    url = models.URLField(unique=True, null=True, blank=True)
+    price = models.CharField(max_length=100, null=True, blank=True)
+    products = models.JSONField()
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Emalls Shop'
+        verbose_name_plural = 'Emalls Shops'
+
+
+class Product(models.Model):
+    related_shop = models.ForeignKey(EmallsShop, on_delete=models.CASCADE, null=True, blank=True)
+    related_shop_token = models.CharField(max_length=100)
+    name = models.CharField(max_length=100)
+    url = models.URLField()
+    image_url = models.URLField()
+    store_count = models.CharField(max_length=100)
+    price = models.CharField(max_length=100)
+    product_id = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+    
+    class Meta:
+        verbose_name = 'Product'
+        verbose_name_plural = 'Products'
+    
