5452d845fc6f8f38c2ddad8acdb1cb1279c14251
Saman-naruee <samannaruee@gmail.com>
2025-01-13 13:53:43 +0330

    keep an Alphabet on shop_informations.

--- emalls_shop/emalls_shop/spiders/bush_me.py
+++ emalls_shop/emalls_shop/spiders/bush_me.py
@@ -23,29 +23,27 @@ class BushMeSpider(scrapy.Spider):
     def parse(self, response):   
         print(Fore.BLUE + f"{response.css('.CommentItem').getall()[0]}" + Style.RESET_ALL)
         shop_informations = {
-            'shop_name': response.css("h1::text").get().replace(" ", "") if response.css("h1::text").get() else None,
-            'shop_current_city': response.css("#ContentPlaceHolder1_lblLocation::text").get(),
-            'phone_number': response.css("#ContentPlaceHolder1_lblTelephone1::text").get(), 
-            'shop_score': response.css("#ContentPlaceHolder1_lblRateValue2::text").get(),
-            'shop_address': response.css("#ContentPlaceHolder1_lblAddress1::text").get(),
-            'shop_website': response.css("#ContentPlaceHolder1_HlkWebsite1::attr(href)").get(),
-            'name_of_person_in_charge': response.css("#ContentPlaceHolder1_lblMasool1::text").get(), 
-            'shop_img_icon': response.css("#ContentPlaceHolder1_imgLogo2::attr(src)").get(),
             'cooperation_status_with_emalls': response.css("#ContentPlaceHolder1_lblshopstatus::text").get(),
-            'shop_duration_of_cooperation_with_emalls': response.css("#ContentPlaceHolder1_lblHamkariBaEmalls::text").get(),
-            'shop_email': response.css("#ContentPlaceHolder1_lblEmail::text").get(),
-            'shop_Enamad_sign': response.css("#ContentPlaceHolder1_lblNamad::text").get(),
-            'shop_social_media_handles': response.css("#ContentPlaceHolder1_DivSocial a::attr(href)").getall(),
-            'shop_crawled_data': response.css("#CtrlFooterLinks_LblDate::text").get(),
+            'name_of_person_in_charge': response.css("#ContentPlaceHolder1_lblMasool1::text").get(), 
+            'phone_number': response.css("#ContentPlaceHolder1_lblTelephone1::text").get(), 
+            'shop_address': response.css("#ContentPlaceHolder1_lblAddress1::text").get(),
             'shop_all_products_target_url': response.css("#DivPartProducts a::attr(href)").get(),
             'shop_comments': [
                     {   
-                        # 'comment_author_image': comment.css(".name img::attr(src)").get(),
                         'comment_date': comment.css(f"#ContentPlaceHolder1_rptComments_lblDate_{i}::text").get(),
-                        # 'comment_text': comment.css("#ContentPlaceHolder1_rptComments_lblComment_4::text").get(),
                     }
                 for i, comment in response.css(".CommentItem")
-            ]
+            ],
+            'shop_crawled_data': response.css("#CtrlFooterLinks_LblDate::text").get(),
+            'shop_current_city': response.css("#ContentPlaceHolder1_lblLocation::text").get(),
+            'shop_duration_of_cooperation_with_emalls': response.css("#ContentPlaceHolder1_lblHamkariBaEmalls::text").get(),
+            'shop_email': response.css("#ContentPlaceHolder1_lblEmail::text").get(),
+            'shop_img_icon': response.css("#ContentPlaceHolder1_imgLogo2::attr(src)").get(),
+            'shop_name': response.css("h1::text").get().replace(" ", "") if response.css("h1::text").get() else None,
+            'shop_score': response.css("#ContentPlaceHolder1_lblRateValue2::text").get(),
+            'shop_social_media_handles': response.css("#ContentPlaceHolder1_DivSocial a::attr(href)").getall(),
+            'shop_website': response.css("#ContentPlaceHolder1_HlkWebsite1::attr(href)").get(),
+            'shop_Enamad_sign': response.css("#ContentPlaceHolder1_lblNamad::text").get(),
         }
         yield shop_informations
         # print(Fore.BLUE + f"{comments}" + Style.RESET_ALL)
