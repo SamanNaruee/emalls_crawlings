24121f6d04d3f7757161d60e6ae1cfa482d65041
Saman-naruee <samannaruee@gmail.com>
2025-01-16 20:57:06 +0330

    update settings: findout that if delay time be too low, may be blocked. update sws spider to handle pagination better.

--- emalls_shop/emalls_shop/spiders/shops_with_specs.py
+++ emalls_shop/emalls_shop/spiders/shops_with_specs.py
@@ -1,5 +1,6 @@
 import scrapy
 from .logger_me import custom_log
+from django.utils import timezone
 
 class ShopsWithSpecsSpider(scrapy.Spider):
     name = "sws"
@@ -15,22 +16,24 @@ class ShopsWithSpecsSpider(scrapy.Spider):
     def parse(self, response):
         final_page = response.css('#ContentPlaceHolder1_rptPagingBottom_hlinkPage_6::text').get()
         total_pages = int(final_page) if final_page and final_page.isdigit() else 3
-        for page in range(1, 3): #Replace 3 with this: total_pages + 1
+        for page in range(1, total_pages + 1): #Replace 3 with this: total_pages + 1
             url = f"https://emalls.ir/Shops/page.{page}"
             yield scrapy.Request(
                 url=url,
                 callback=self.page_detail_parse,
+                meta={'current_page':page}
             )
 
     def page_detail_parse(self, response):
+        current_page = response.meta['current_page']
         shop_divs = response.css('div.row > div')
         total_shops = len(shop_divs)
-        
-        for id in range(total_shops):
+        for id in range(48):
             shop_url_partial = response.css(f"#ContentPlaceHolder1_rptShops_hlkTitle_{id}::attr(href)").get()
             if shop_url_partial:
                 full_shop_url = f'https://emalls.ir/{shop_url_partial}'
                 shop = {
+                    'shop_was_in_page': current_page,
                     'shop_img': response.css(f'#ContentPlaceHolder1_rptShops_imgLogo_{id}::attr(src)').get(),
                     'shop_title': response.css(f"#ContentPlaceHolder1_rptShops_hlkTitle_{id}::text").get(),
                     'shop_url': full_shop_url,
@@ -66,7 +69,8 @@ class ShopsWithSpecsSpider(scrapy.Spider):
                 } 
                 for i, comment in enumerate(response.css(".CommentItem"))
             ],
-            'shop_crawled_date': response.css("#CtrlFooterLinks_LblDate::text").get(),
+            'shop_recieved_date': response.css("#CtrlFooterLinks_LblDate::text").get(),
+            'shop_crawled_at': timezone.now(),
             'shop_current_city': response.css("#ContentPlaceHolder1_lblLocation::text").get(),
             'shop_duration_of_cooperation_with_emalls': response.css("#ContentPlaceHolder1_lblHamkariBaEmalls::text").get(),
             'shop_img_icon': response.css("#ContentPlaceHolder1_imgLogo2::attr(src)").get(),
