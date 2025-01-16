5e38b94db60bc0e426a3d653bc1fc85feb3f85f9
Saman-naruee <samannaruee@gmail.com>
2025-01-16 11:06:55 +0330

    upate emalls_api to handle more detail info. still pagination is hardcoded.

--- emalls_shop/emalls_shop/spiders/emalls_api.py
+++ emalls_shop/emalls_shop/spiders/emalls_api.py
@@ -1,5 +1,8 @@
 import scrapy  
 import json  
+from .logger_me import custom_log
+import datetime  
+
 
 class EmallsApiSpider(scrapy.Spider):  
     name = "emalls_api"  
@@ -21,6 +24,7 @@ class EmallsApiSpider(scrapy.Spider):
                 url=self.start_urls[0],  
                 method='POST',  
                 formdata=form_data,  
+                meta={'shop_id': form_data['shop'], 'page': page},
                 callback=self.parse  
             )  
 
@@ -28,13 +32,14 @@ class EmallsApiSpider(scrapy.Spider):
         # Load the JSON data from the response  
         data = json.loads(response.body)  
         data = data.get('lstsearchresualt', [])
+        formdata = json.loads(response.body)
         products = []   
         for product in data:
             sent_product = {
                 'id': product.get('id'),  
                 'title': product.get('title'),  
                 'titlefa': product.get('titlefa'),  
-                'link': product.get('link'),  
+                'link': f"https://emalls.ir/{product.get('link')}",  
                 'image': product.get('image'),  
                 'category': product.get('category'),  
                 'rate': product.get('rate'),  
@@ -47,10 +52,14 @@ class EmallsApiSpider(scrapy.Spider):
                 'buyTitle': product.get('buyTitle'),  
                 'buyLink': product.get('buyLink'),  
                 'spec': product.get('spec'),  
-                'used': product.get('used'),                  
+                'used': product.get('used'),  
+                'shop_id': response.meta['shop_id'],
+                'crawled_date': datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'),
+                'page': response.meta['page'],   
             }
             
             products.append(sent_product)
+
         yield {
             'products': products,
         }
