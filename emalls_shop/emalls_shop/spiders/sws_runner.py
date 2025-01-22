1e0130fd64ac3b08a502912e81042ec450576df6
Saman-naruee <samannaruee@gmail.com>
2025-01-22 13:57:15 +0330

    update script.

--- emalls_shop/emalls_shop/spiders/sws_runner.py
+++ emalls_shop/emalls_shop/spiders/sws_runner.py
@@ -1,7 +1,7 @@
 import multiprocessing, os, sys
 from scrapy.crawler import CrawlerProcess
 from scrapy.utils.project import get_project_settings
-from shops_with_specs import ShopsWithSpecsSpider
+from .shops_with_specs import ShopsWithSpecsSpider
 
 
 """
@@ -16,7 +16,8 @@ Arguments:
     this number into multiple processes to optimize crawling based on the available CPU cores.
 
 Example:
-    python sws_runner.py 300
+    cd "E:\saman\emalls\emalls_shop"
+    python -m emalls_shop.spiders.sws_runner 600
 
 Note:
     Ensure that this script is in the same directory as the ShopsWithSpecsSpider 
@@ -48,7 +49,7 @@ if __name__ == "__main__":
         process = multiprocessing.Process(
             target=sws_runner,
             args=(end_page - start_page + 1,),
-            kwargs={'pages': end_page - start_page + 1}
+            # kwargs={'pages': end_page - start_page + 1}
         )
         process.start()
         processes.append(process)
@@ -56,12 +57,21 @@ if __name__ == "__main__":
     for process in processes:
         process.join()
     
-    with open(f"sws/{pages // 100}.csv", "w") as f:
-        f.write("shop_id,shop_title,shop_url,shop_selling_type,shop_was_in_page,shop_img,shop_score,shop_social_media_handles,shop_recieved_date,shop_crawled_at,shop_current_city,shop_duration_of_cooperation_with_emalls,shop_website,shop_Enamad_sign,name_of_person_in_charge,phone_number,shop_email,shop_address,senfi_number,cooperation_status_with_emalls,shop_all_products_target_url\n")
-        for i in range(start_page, end_page + 1):
-            with open(f"sws/{i}.json", "r") as f2:
-                for line in f2:
-                    f.write(line.strip() + "\n")
+    # create the sws directory if it does not exist  
+    if not os.path.exists("sws"):  
+        os.makedirs("sws")  
+
+    with open(f"sws/{pages // 100}.csv", "w") as f:  
+        f.write("shop_id,shop_title,shop_url,shop_selling_type,shop_was_in_page,shop_img,shop_score,shop_social_media_handles,shop_recieved_date,shop_crawled_at,shop_current_city,shop_duration_of_cooperation_with_emalls,shop_website,shop_Enamad_sign,name_of_person_in_charge,phone_number,shop_email,shop_address,senfi_number,cooperation_status_with_emalls,shop_all_products_target_url\n")  
+        
+        for i in range(start_page, end_page + 1):  
+            json_file_path = f"sws/{i}.json"  
+            if os.path.exists(json_file_path):  
+                with open(json_file_path, "r") as f2:  
+                    for line in f2:  
+                        f.write(line.strip() + "\n")  
+            else:  
+                print(f"Warning: {json_file_path} does not exist.")  
     
     
     # remove temp json files after merging into one big csv file
