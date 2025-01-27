70008b6eb0886c7c23e6410d5ff8211ee9be25fa
Saman-naruee <samannaruee@gmail.com>
2025-01-27 12:26:08 +0330

    Reduce security settigns.

--- emalls_shop/emalls_shop/spiders/products.py
+++ emalls_shop/emalls_shop/spiders/products.py
@@ -13,23 +13,11 @@ class ProductsSpider(scrapy.Spider):
     """
     custom_settings = {
         'RETRY_ENABLED': True,  
-        'RETRY_TIMES': 3,
-        'DOWNLOAD_DELAY': 0.025,                    # 25 milisecond delay between requests
-        'CONCURRENT_REQUESTS': 12,                  # max number of concurrent requests on all domains
-        'DOWNLOAD_TIMEOUT': 30,                     # max time in second to wait for a response
-        'REACTOR_THREADPOOL_MAXSIZE': 10,           # max size of twisted reactor thread pool
-        'LOG_LEVEL': 'INFO',                        # level of logging detail ((DEBUG, INFO, WARNING, ERROR, CRITICAL)): less termnal output
-        'COOKIES_ENABLED': False,                   # disable cookie handling to reduce overhead
-        'RETRY_ENABLED': False,                     # disable automatic retry of failed requests
-        'DOWNLOAD_FAIL_ON_DATALOSS': False,         # handle fail of incomplete responses
-        'AUTOTHROTTLE_ENABLED': True,
-        'AUTOTHROTTLE_START_DELAY': 0.025,             # initial delay
-        'AUTOTHROTTLE_MAX_DELAY': 0.75,                # max delay
-        'AUTOTHROTTLE_TARGET_CONCURRENCY': 1.0,        # target concurrency
-        'DOWNLOADER_MIDDLEWARES': {  
-            'scrapy.downloadermiddlewares.useragent.UserAgentMiddleware': None,  
-            'scrapy_user_agents.middlewares.RandomUserAgentMiddleware': 10,  
-        }, 
+        'RETRY_TIMES': 1,
+        'DOWNLOAD_DELAY': 0.5,                       # 1 second delay between requests
+        'DOWNLOAD_TIMEOUT': 30,                    # max time in second to wait for a response
+        'LOG_LEVEL': 'INFO',                       # level of logging detail ((DEBUG, INFO, WARNING, ERROR, CRITICAL)): less termnal output
+        'DOWNLOAD_FAIL_ON_DATALOSS': True,         # handle fail of incomplete responses
     }
     
     
--- emalls_shop/emalls_shop/spiders/python_crawl.py
+++ emalls_shop/emalls_shop/spiders/python_crawl.py
@@ -1,39 +1,43 @@
-from operator import truediv
-import io, sys
-import requests
-from urllib.parse import urlencode
-import json
+# from operator import truediv
+# import io, sys
+# import requests
+# from urllib.parse import urlencode
+# import json
+# import random
+# from time import sleep
 
-sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding='utf-8')
+# sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding='utf-8')
 
-def crawl_emalls(token="1"):
-    pagenum = 1
-    while pagenum:
-        form_data = {
-            "entekhab": "listitemv2",
-            "currenturl": f"https://emalls.ir/%d9%84%db%8c%d8%b3%d8%aa-%d9%82%db%8c%d9%85%d8%aa~shop~{token}~page~{pagenum}",
-            "pagenum": str(pagenum),
-            "shop": token,
-            "sort": "1",
-            "order": "1",
-            "view": "1"
-        }
-        headers = {
-            "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
-            "X-Requested-With": "XMLHttpRequest",
-            "Accept": "application/json, text/javascript, */*; q=0.01",
-            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; rv:78.0) Gecko/20100101 Firefox/78.0"  # 200 means success
-        }
-        response = requests.post("https://emalls.ir/_Search.ashx", data=urlencode(form_data), headers=headers)
-        print("Status Code:", response.status_code)
-        response_body = response.json()
+# def crawl_emalls(token="1"):
+#     pagenum = 1
+#     while pagenum:
+#         # Sleep for just page
+#         sleep(random.randrange(0.9, 2.0))
+#         form_data = {
+#             "entekhab": "listitemv2",
+#             "currenturl": f"https://emalls.ir/%d9%84%db%8c%d8%b3%d8%aa-%d9%82%db%8c%d9%85%d8%aa~shop~{token}~page~{pagenum}",
+#             "pagenum": str(pagenum),
+#             "shop": token,
+#             "sort": "1",
+#             "order": "1",
+#             "view": "1"
+#         }
+#         headers = {
+#             "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
+#             "X-Requested-With": "XMLHttpRequest",
+#             "Accept": "application/json, text/javascript, */*; q=0.01",
+#             "User-Agent": "Mozilla/5.0 (Windows NT 10.0; rv:78.0) Gecko/20100101 Firefox/78.0"  # 200 means success
+#         }
+#         response = requests.post("https://emalls.ir/_Search.ashx", data=urlencode(form_data), headers=headers)
+#         print("Status Code:", response.status_code)
+#         response_body = response.json()
 
-        with open(f"Digikala_shop_page_{pagenum}.json", "w", encoding="utf-8") as f:
-            json.dump(response_body, f, ensure_ascii=False, indent=4)
-            print(f"Successfully crawled page: {pagenum} at {response_body['pagetitle']}")
+#         with open(f"Digikala_shop_page_{pagenum}.json", "w", encoding="utf-8") as f:
+#             json.dump(response_body, f, ensure_ascii=False, indent=4)
+#             print(f"Successfully crawled page: {pagenum} at {response_body['pagetitle']}")
 
-        pagenum = response_body['lstpagingresualt'][-1]['page_number'] if (
-            response_body['lstpagingresualt'] and response_body['lstpagingresualt'][-1]['stat'] == 'next') else None
+#         pagenum = response_body['lstpagingresualt'][-1]['page_number'] if (
+#             response_body['lstpagingresualt'] and response_body['lstpagingresualt'][-1]['stat'] == 'next') else None
 
-if __name__ == "__main__":
-    crawl_emalls(token="1")
+# if __name__ == "__main__":
+#     crawl_emalls(token="1")
