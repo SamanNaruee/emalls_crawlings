3ca491e1e46b7a3aba1c6412eaee179f1c9da13e
Saman-naruee <samannaruee@gmail.com>
2025-02-01 13:37:23 +0330

    Update custom settings to better performance: got 1350 products in just 25 minutes!.

--- emalls_shop/emalls_shop/spiders/products.py
+++ emalls_shop/emalls_shop/spiders/products.py
@@ -14,8 +14,8 @@ class ProductsSpider(scrapy.Spider):
     custom_settings = {
         'RETRY_ENABLED': True,  
         'RETRY_TIMES': 3,
-        'DOWNLOAD_DELAY': 0.50,                    # 25 milisecond delay between requests
-        'CONCURRENT_REQUESTS': 12,                  # max number of concurrent requests on all domains
+        'DOWNLOAD_DELAY': 0.40,                    # 25 milisecond delay between requests
+        'CONCURRENT_REQUESTS': 13,                  # max number of concurrent requests on all domains
         'DOWNLOAD_TIMEOUT': 30,                     # max time in second to wait for a response
         'REACTOR_THREADPOOL_MAXSIZE': 10,           # max size of twisted reactor thread pool
         'LOG_LEVEL': 'INFO',                        # level of logging detail ((DEBUG, INFO, WARNING, ERROR, CRITICAL)): less termnal output
@@ -23,8 +23,8 @@ class ProductsSpider(scrapy.Spider):
         'RETRY_ENABLED': False,                     # disable automatic retry of failed requests
         'DOWNLOAD_FAIL_ON_DATALOSS': False,         # handle fail of incomplete responses
         'AUTOTHROTTLE_ENABLED': True,
-        'AUTOTHROTTLE_START_DELAY': 0.50,             # initial delay
-        'AUTOTHROTTLE_MAX_DELAY': 0.75,                # max delay
+        'AUTOTHROTTLE_START_DELAY': 0.40,             # initial delay
+        'AUTOTHROTTLE_MAX_DELAY': 0.65,                # max delay
         'AUTOTHROTTLE_TARGET_CONCURRENCY': 1.0,        # target concurrency
         'DOWNLOADER_MIDDLEWARES': {  
             'scrapy.downloadermiddlewares.useragent.UserAgentMiddleware': None,  
